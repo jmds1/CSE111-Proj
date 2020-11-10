3f2086ffb91d43d9afc2e1442aaf29cd71cfd1b2
Bernadette Ortiz <47306822+BernadetteOrtiz@users.noreply.github.com>
2020-11-09 21:28:44 -0800

    finished plant info table

--- tables.sql
+++ tables.sql
@@ -68,8 +68,8 @@ CREATE TABLE search(
 --Insert Sample Data for user account 
 INSERT INTO userAccount(user_idN, user_username, user_password, user_email, user_fullName, user_uLocation, user_lastLogin)
 VALUES(01, 'jmds', '1234', 'jmds@gmail.com', 'Jesus Martinez', 5342, 3), 
-        (02, 'Joce', 'cx12', 'jocea@gmail.com', 'Jocelyn Aguila', 3232, 400),
-        (03, 'marels', 'horse', 'marelsa@gmail.com', 'Marilyn Angeles', 3012, 5),
+        (02, 'Joce', 'cx12', 'jocea@gmail.com', 'Joce Aguila', 3232, 400),
+        (03, 'marels', 'horse', 'marelsa@gmail.com', 'Marels Angeles', 3012, 5),
         (04, 'Ale', 'bottle', 'alech@gmail.com', 'Ale Chavez', 4321, 2),
         (05, 'Eddie', 'LOL', 'eddief@gmail.com', 'Eddie Flores', 1234, 4),
         (06, 'Vero', 'yuumi', 'verol@gmail.com', 'Vero Lopez', 1234, 17),
@@ -80,8 +80,23 @@ VALUES(01, 'jmds', '1234', 'jmds@gmail.com', 'Jesus Martinez', 5342, 3),
 
 --Insert Sample Data for plants info
 INSERT INTO plantInfo(plant_id, plant_type, plant_name, plant_soil, plant_watering, plant_height, plant_food, plant_environment)
-VALUES(01,'Cactus', 'San pedro', 'sand', 1, 'mixed', 12, 'Hot and dry'),
-        (02,'Cactus', 'San pedro', 'sand', 1, 'mixed', 12, 'Hot and dry')
+VALUES(01,'Cactus', 'San pedro', 'sand', 1, 12, 'mixed', 'Hot and dry'),
+        (02,'Cactus', 'Organ pipe', 'sand', 1, 5, 'mixed', 'Hot and dry'),
+        (03,'Cactus', 'Rattail', 'sand', 1, 20, 'mixed', 'Hot and dry'),
+        (04,'Rose', 'Red Rose', 'loam', 2, 6, 'powdered', 'Sunny and moist'),
+        (05,'Rose', 'Beach Rose', 'loam', 3, 11, 'powdered', 'Sunny and moist'),
+        (06,'Rose', 'French Rose', 'loam', 2, 10, 'powdered', 'Sunny and moist'),
+        (07,'Rose', 'Cabbage Rose', 'loam', 3, 1, 'powdered', 'Sunny and moist'),
+        (08,'Vascular Plant', 'Black Locust', 'mixed', 1, 2, 'none', 'Shaded and moist'),
+        (09,'Vascular Plant', 'Whisk Fern', 'mixed', 2, 15, 'none', 'Shaded and moist'),
+        (10,'Vascular Plant', 'Thale cress', 'mixed', 1.5, 4, 'none', 'Shaded and moist'),
+        (11,'Vascular Plant', 'Betula Nana', 'mixed', 1, 9, 'none', 'Shaded and moist'),
+        (12,'Succulent', 'Jade', 'sand', .5, 7, 'liquid', 'Sunny and dry'),
+        (13,'Succulent', 'Jelly Bean', 'sand', 1, 4, 'liquid', 'Sunny and dry'),
+        (14,'Succulent', 'Chocolate Soldier', 'sand', .25, 3, 'liquid', 'Sunny and dry'),
+        (15,'Succulent', 'Ghost Plant', 'sand', .25, 2, 'liquid', 'Sunny and dry'),
+
+
 
 ;
 
