6d1e4d27b11e89d3244c9b0daf85ffb632b0b950
Jesus Martinez-de Santos <jesusmartinez939@gmail.com>
2020-11-09 18:31:55 -0800

    Wrote more queries

--- query.sql
+++ query.sql
@@ -10,4 +10,26 @@
 
 --#6 Find which archived plants can survive in all seasons.
 
---#7 
+--#7 We want to clear up some data form the system, we want to remove all users whose last login was more than 257 days ago.
+
+--#8 Find the id of plants who are above the average height of all plants
+
+--#9 What products does the shop closest to user 08 carry?
+
+--#10 What plants can have mixed soil and a height of less than 10?
+
+--#11 Sadly the plant shop HomeGrown had to downsize and relocated approximatley 23 miles from their last location, reflect this change.
+
+--#12 List user id of users who have the same locaiton, haved logged in within 15 days and archived a succulent or cactus plant.
+
+--#13 Find which users have made more than 3 searches in the past 5 days where at least two of them were for plants of species Vascular plant. 
+
+--#14 How many plants need to be watered at least 2 times per week.
+
+--#15 User 01 is embarred by the post they made, they want to delete all of them, make this change.user_username
+
+--#16 The app gained popularity, we need to add 5 more users
+
+--#17 Find the tallest plant that requires soil... List the name of the plant and the name of the shops that carry that soil.
+
+--#18 The latest post of user 02 just went viral, update like number to reflect this, 1000000 likes.
